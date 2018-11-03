84f411a6b50b2edb1358edcefeaa78640a8f7433
Swati Pruthi <swati.pruthi@accoliteindia.com>
2018-11-03 17:45:59 +0530

    add audio record feature

--- src/App.js
+++ src/App.js
@@ -14,23 +14,52 @@ class App extends Component {
       .then(res => res.json())
       .then(res => console.log(res));
   }
+  stopRecording = () => {
+    fetch("https://avatar.lyrebird.ai/api/v0/generate", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer oauth_1CObEa9iKp4lcqwAjrFMxMO98i0"
+      },
+      method: "POST",
+      body: JSON.stringify({ text: "hello" })
+    })
+      .then(res => res.json())
+      .then(res => console.log(res));
+  };
+  startRecording = () => {
+    let audio = document.getElementById("start");
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then(stream => {
+        console.log(stream);
+        audio.src = stream;
+        var context = new AudioContext();
+        var source = context.createMediaStreamSource(audio.src);
+        var processor = context.createScriptProcessor(1024, 1, 1);
+        source.connect(processor);
+        processor.connect(context.destination);
+
+        processor.onaudioprocess = function(e) {
+          console.log(e.inputBuffer);
+          context.close();
+          // Do something with the data, i.e Convert this to WAV
+        };
+      })
+      .catch(error => console.log(error));
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div />
         </header>
+        <button id="start" onClick={this.startRecording}>
+          start{" "}
+        </button>
+        <button id="stop" onClick={this.stopRecording}>
+          Stop
+        </button>
       </div>
     );
   }
